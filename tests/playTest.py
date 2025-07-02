f8d9a01d675fa3416a9fbd23d461440805806f8d
Mike2147483647 <chanmike268124@gmail.com>
2025-07-02 19:24:06 +0100

    updated README.md for this week's meeting and preprocessed the fully labelled data for ML training later in data/data

--- tests/playTest.py
+++ tests/playTest.py
@@ -1,16 +1,18 @@
-import birdwinglabel.data.data as birdData
+from birdwinglabel.data.data import full_bilateral_markers, bilateral_markers, bilateral_frame, full_no_labels
 import pandas as pd
 
-# # checking the head of the loaded dataframes and arrays
-# print(birdData.full_bilateral_markers.info())
-# # print(birdData.bilateral_markers[:5])
-# # full_bilateral_colnames = birdData.full_bilateral_markers.columns.tolist()
-# # print(full_bilateral_colnames)
+# checking the head of the loaded dataframes and arrays
+print(full_bilateral_markers.info())
+print(full_bilateral_markers.columns.tolist())
+# print(bilateral_markers[:5])
+# full_bilateral_colnames = birdData.full_bilateral_markers.columns.tolist()
+# print(full_bilateral_colnames)
 # print(birdData.full_no_labels.info())
 # print(birdData.bilateral_frame.info())
 # print(birdData.bilateral_frame[birdData.bilateral_frame['seqID'] == '04_09_038_1'])
 
-df = pd.DataFrame({'A': [3, 1, 2], 'B': [9, 8, 7]})
-df["A"] = pd.Categorical(df['A'], categories=[2,1,3], ordered=True)
-df = df.sort_values('A').reset_index(drop=True)
-print(df)
+
+# df = pd.DataFrame({'A': [3, 1, 2], 'B': [9, 8, 7]})
+# df["A"] = pd.Categorical(df['A'], categories=[2,1,3], ordered=True)
+# df = df.sort_values('A').reset_index(drop=True)
+# print(df)
